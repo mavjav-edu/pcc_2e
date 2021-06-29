e01804d5ddbb1540178d3613b1ccbf1a3dcd6300
Mavaddat Javid <mavaddat.javid@gmail.com>
2021-06-28 18:25:25 -0700

    prelim

---
+++ setreadme.ps1
@@ -0,0 +1,51 @@
+#Requires -PSEdition Core
+$TESTING = $false
+
+$book = [System.IO.File]::ReadAllBytes("C:\Users\MAVADD~1\CALIBR~1\CALIBR~1\ERICMA~2\PY98C8~1\Python Crash Course_ A Hands-On - Eric Matthes.epub")
+[System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression')
+$zipStream = New-Object System.IO.Memorystream
+$zipStream.Write($book, 0, $book.Count) | Out-Null
+$zipFile = [System.IO.Compression.ZipArchive]::new($zipStream)
+$htmlFiles = $zipFile.Entries | Where-Object -Property FullName -Match "ch\d+\.html"
+foreach ($htmlFile in $htmlFiles) {
+    $chapter = $zipFile.GetEntry($htmlFile)
+    $chapter.Open().Read(($chContentBytes = [byte[]]::new($chapter.Length)), 0, $($chapter.Length)) | Out-Null
+    $chContentStr = [System.Text.Encoding]::Default.GetString($chContentBytes)
+    $HTML = New-Object -Com "HTMLFile"
+    $HTML.write([ref]$chContentStr) | Out-Null
+    $chNum = (Select-String -InputObject $htmlFile.Name -Pattern "(\d+)").Matches.Value
+    $README_md = "chapter_$chNum\README.md"
+    # $chTitle has XPath //*[@id="ch$chNum"]/strong[2]
+    $chTitle = (Get-Culture).TextInfo.ToTitleCase($HTML.getElementById("ch$chNum").getElementsByTagName('strong')[1].innerHTML.ToLowerInvariant())
+    $TryItYourselfs = $HTML.getElementsByClassName('sidebar')
+    $i = 0
+    $outStr = Out-String -InputObject ("<H1>" + $chTitle + "</H1>`n" | pandoc @("--from=HTML", "--to=markdown_mmd+backtick_code_blocks+fenced_code_blocks+autolink_bare_uris"))
+    $outStr = @"
+
+
+$outStr
+
+"@
+
+    if ($TESTING) {
+        $tempFile = New-TemporaryFile
+        Out-File -InputObject $outStr -FilePath  $tempFile -Encoding utf8
+        if (Test-Path -Path $README_md) { Get-Content $README_md | Out-File -FilePath $tempFile -Append }
+        Move-Item -Path $tempFile -Destination $README_md -Force
+    }
+    else {
+        Out-File -InputObject $outStr -FilePath  $README_md -Append -Encoding utf8
+    }
+    foreach ($TryItYourself in $TryItYourselfs) {
+        $i++
+        $outStr = Out-String -InputObject (("<BR/>" + (Out-String -InputObject $TryItYourself.innerHTML) -creplace '<STRONG>TRY IT YOURSELF</STRONG>', "<H2>TRY IT YOURSELF &#35;$i</H2>" -replace '<([^>\s]+)\s*class="?programs"?>([\s\S]+?)<\/\1>', '<pre class="python"><code>$2</code></pre>' -replace '<span\s*class="?literal"?>([\s\S]+?)</span>', '<code>$1</code>' | pandoc @("--from=HTML", "--to=markdown_mmd+backtick_code_blocks+fenced_code_blocks+autolink_bare_uris")) -replace 'ch(\d+)\.html', '../chapter_$1/README.md')
+
+        if ($TESTING) {
+            Out-Host -InputObject $outStr
+            pause
+        }
+        else {
+            Out-File -InputObject $outStr -FilePath  $README_md -Append -Encoding utf8
+        }
+    }
+}
---
+++ setreadme_appendices.ps1
@@ -0,0 +1,45 @@
+#Requires -PSEdition Core
+$TESTING = $true
+
+$book = [System.IO.File]::ReadAllBytes("*.epub")
+[System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression')
+$zipStream = New-Object System.IO.Memorystream
+$zipStream.Write($book, 0, $book.Count) | Out-Null
+$zipFile = [System.IO.Compression.ZipArchive]::new($zipStream)
+$htmlFiles = $zipFile.Entries | Where-Object -Property FullName -Match "app\d+\.html"
+foreach ($htmlFile in $htmlFiles) {
+    $appendix = $zipFile.GetEntry($htmlFile)
+    $appendix.Open().Read(($appContentBytes = [byte[]]::new($appendix.Length)), 0, $($appendix.Length)) | Out-Null
+    $appContentStr = [System.Text.Encoding]::Default.GetString($appContentBytes)
+    $HTML = New-Object -Com "HTMLFile"
+    $HTML.write([ref]$appContentStr) | Out-Null
+    $numToLetter = @{0='';1="a";2="b";3="c";4="d"}
+    $appNum = (Select-String -InputObject $htmlFile.Name -Pattern "(\d+)").Matches.Value
+    $appLetter = $numToLetter[[Int32]::Parse($appNum)]
+    $README_md = "appendix_$appLetter\README.md"
+    # $appTitle has XPath //*[@id="app$appNum"]/strong[2]
+    if(-not (Test-Path -Path $README_md -ErrorAction Continue) -and ($HTML.getElementById("app$appNum").getElementsByTagName('strong').Count -lt 2)){
+        continue
+    }
+    $appTitle = (Get-Culture).TextInfo.ToTitleCase($HTML.getElementById("app$appNum").getElementsByTagName('strong')[1].innerHTML.ToLowerInvariant())
+    $outStr = Out-String -InputObject ("<H1>" + $appTitle + "</H1>`n" | pandoc @("--from=HTML", "--to=markdown_mmd+backtick_code_blocks+fenced_code_blocks+autolink_bare_uris"))
+    $outStr = @"
+
+
+$outStr
+
+"@;
+
+    if ($TESTING) {
+        $tempFile = New-TemporaryFile
+        Out-File -InputObject $outStr -FilePath  $tempFile -Encoding utf8
+        Out-File -FilePath $tempFile -Encoding utf8 -Append -InputObject ($HTML.ie9_body | pandoc @("--from=HTML", "--to=markdown_mmd+backtick_code_blocks+fenced_code_blocks+autolink_bare_uris"))
+        #Get-Content $README_md | Out-File -FilePath $tempFile -Append
+
+        Move-Item -Path $tempFile -Destination $README_md -Force
+
+    }
+    else {
+        Out-File -InputObject $outStr -FilePath  $README_md -Append -Encoding utf8
+    }
+}
