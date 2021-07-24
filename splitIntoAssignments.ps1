8083ad0cc7dabceb3b0a9036837bca27c5a415f1
Mavaddat Javid <mavaddat.javid@gmail.com>
2021-07-23 18:04:04 -0700

    script debug feedback

--- splitIntoAssignments.ps1
+++ splitIntoAssignments.ps1
@@ -1,13 +1,34 @@
 Get-ChildItem -Path "." -Filter "chapter*" -Directory | ForEach-Object { 
     $chStr = (Select-String -InputObject $_.BaseName -Pattern "chapter_(\d+)").Matches.Groups[1]
-    New-Item -Path $env:TEMP -ItemType Directory -Name "pcc-chapter-$chStr"
-    Push-Location -Path "$env:TEMP\pcc-chapter-$chStr"
-    gh repo clone "mavaddat-javid-education/pcc-chapter-$chStr"
+    if(Test-Path "$env:TEMP\pcc-chapter-$chStr" -PathType Container ) {
+        Remove-Item "$env:TEMP\pcc-chapter-$chStr" -Force -Recurse -ErrorAction Ignore
+    }
+    Write-Host "Processing chapter $chStr"
+    Push-Location $env:TEMP
+    git clone "https://github.com/mavaddat-javid-education/pcc-chapter-$chStr.git"
+    Push-Location -Path ".\pcc-chapter-$chStr" -ErrorAction Break
+    Write-Host "Removing the chapter folder"
     git rm -r *
-    Copy-Item -Path $_ -Recurse -Destination .
+    if (-not $?) {
+        throw "Error with git remove!"
+    }
+    Write-Host "Copying to the chapter folder"
+    Copy-Item -Path $_ -Recurse -Destination . -ErrorAction Break
+    Write-Host "Staging the chapter"
     git add *
+    if (-not $?) {
+        throw "Error with git add!"
+    }
+    Write-Host "Committing the chapter"
     git commit -a -m "update to pcc_2e"
-    git push origin master
+    if (-not $?) {
+        throw "Error with git commit!"
+    }
+    Write-Host "Pushing the chapter"
+    git push --force
+    if (-not $?) {
+        throw "Error with git push!"
+    }
     #gh repo create "mavaddat-javid-education/pcc-chapter-$chStr" -d="$title`: Assignment for Ch $chInt in Python Crash Course" --enable-issues=false --enable-wiki=false --confirm --public
     Pop-Location
 }
