441552345d04c293db8a79eeb638431923d111f4
mavaddat <javid@mavaddat.ca>
2021-11-18 17:50:46 +0000

    Deploying to gh-pages from  @ f9fe95bdc6597ac06ef2c4aa801b92ad6bdf8ffb 🚀

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -1,5 +1,3 @@
----
----
 (function (jtd, undefined) {
 
 // Event handling
@@ -55,40 +53,24 @@ function initNav() {
       mainHeader.classList.remove('nav-open');
     }
   });
-
-  {%- if site.search_enabled != false and site.search.button %}
-  const searchInput = document.getElementById('search-input');
-  const searchButton = document.getElementById('search-button');
-
-  jtd.addEvent(searchButton, 'click', function(e){
-    e.preventDefault();
-
-    mainHeader.classList.add('nav-open');
-    searchInput.focus();
-  });
-  {%- endif %}
 }
-
-{%- if site.search_enabled != false %}
 // Site search
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', '{{ "assets/js/search-data.json" | absolute_url }}', true);
+  request.open('GET', 'https://mavjav-edu.github.io/pcc_2e/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
       var docs = JSON.parse(request.responseText);
       
-      lunr.tokenizer.separator = {{ site.search.tokenizer_separator | default: site.search_tokenizer_separator | default: "/[\s\-/]+/" }}
+      lunr.tokenizer.separator = /[\s\-/]+/
 
       var index = lunr(function(){
         this.ref('id');
         this.field('title', { boost: 200 });
         this.field('content', { boost: 2 });
-        {%- if site.search.rel_url != false %}
         this.field('relUrl');
-        {%- endif %}
         this.metadataWhitelist = ['position']
 
         for (var i in docs) {
@@ -96,9 +78,7 @@ function initSearch() {
             id: i,
             title: docs[i].title,
             content: docs[i].content,
-            {%- if site.search.rel_url != false %}
             relUrl: docs[i].relUrl
-            {%- endif %}
           });
         }
       });
@@ -261,7 +241,7 @@ function searchLoaded(index, docs) {
             var previewEnd = position[0] + position[1];
             var ellipsesBefore = true;
             var ellipsesAfter = true;
-            for (var k = 0; k < {{ site.search.preview_words_before | default: 5 }}; k++) {
+            for (var k = 0; k < 5; k++) {
               var nextSpace = doc.content.lastIndexOf(' ', previewStart - 2);
               var nextDot = doc.content.lastIndexOf('. ', previewStart - 2);
               if ((nextDot >= 0) && (nextDot > nextSpace)) {
@@ -276,7 +256,7 @@ function searchLoaded(index, docs) {
               }
               previewStart = nextSpace + 1;
             }
-            for (var k = 0; k < {{ site.search.preview_words_after | default: 10 }}; k++) {
+            for (var k = 0; k < 10; k++) {
               var nextSpace = doc.content.indexOf(' ', previewEnd + 1);
               var nextDot = doc.content.indexOf('. ', previewEnd + 1);
               if ((nextDot >= 0) && (nextDot < nextSpace)) {
@@ -336,7 +316,7 @@ function searchLoaded(index, docs) {
         resultLink.appendChild(resultPreviews);
 
         var content = doc.content;
-        for (var j = 0; j < Math.min(previewPositions.length, {{ site.search.previews | default: 3 }}); j++) {
+        for (var j = 0; j < Math.min(previewPositions.length, 3); j++) {
           var position = previewPositions[j];
 
           var resultPreview = document.createElement('div');
@@ -352,13 +332,10 @@ function searchLoaded(index, docs) {
           }
         }
       }
-
-      {%- if site.search.rel_url != false %}
       var resultRelUrl = document.createElement('span');
       resultRelUrl.classList.add('search-result-rel-url');
       resultRelUrl.innerText = doc.relUrl;
       resultTitle.appendChild(resultRelUrl);
-      {%- endif %}
     }
 
     function addHighlightedText(parent, text, start, end, positions) {
@@ -448,7 +425,6 @@ function searchLoaded(index, docs) {
     }
   });
 }
-{%- endif %}
 
 // Switch theme
 
@@ -459,19 +435,24 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', '{{ "assets/css/just-the-docs-" | absolute_url }}' + theme + '.css');
+  cssFile.setAttribute('href', 'https://mavjav-edu.github.io/pcc_2e/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Document ready
 
 jtd.onReady(function(){
   initNav();
-  {%- if site.search_enabled != false %}
   initSearch();
-  {%- endif %}
 });
 
 })(window.jtd = window.jtd || {});
 
-{% include js/custom.js %}
+// make email imgs as tall as the height of the line height
+for (let emailImg of document.getElementsByClassName('email')) {  
+  emailImg.style.lineHeight = parseInt(window.getComputedStyle(emailImg.parentElement).getPropertyValue('line-height')) + 'px';
+  //calculate the font size of surrounding text
+  emailImg.style.fontSize = parseInt(window.getComputedStyle(emailImg.parentElement).getPropertyValue('font-size')) + 'px';
+  emailImg.style.maxHeight = emailImg.style.fontSize;
+}
+
 
